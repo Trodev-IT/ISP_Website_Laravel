5cb74628639953a0137af50fc22656dbc1a44a37
rakib-utsho <rakibutsho1920@gmail.com>
2024-02-25 16:22:07 +0600

    update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function aboutus()
+    {
+        return view( 'about' );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function package()
+    {
+       return view('package');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function features()
+    {
+        return view( 'features' );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function blog()
+    {
+        return view( 'blog' );
+    }
+
+    public function blogsdetails()
+    {
+        return view('blogsdetails');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[UserController::class,'welcome'])->name('welcome');
+
+Route::get('/aboutus',[UserController::class, 'aboutus']) -> name ('aboutus');
+Route::get('/package',[UserController::class, 'package']) -> name ('packages');
+Route::get('/features',[UserController::class, 'features']) -> name ('features');
+Route::get('/blog',[UserController::class, 'blog']) -> name ('blogs');
+Route::get('/blogsdetails',[UserController::class'blogsdetails'])->name( 'blogsdetails');
+
