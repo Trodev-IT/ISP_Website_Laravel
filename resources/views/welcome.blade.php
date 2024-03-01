fa055bd29c8e104a5d9ddd5e94f750473baba8b1
rubayet <rubayetislam16@gmail.com>
2024-03-02 01:06:50 +0600

    update by rubayet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,23 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}">
 
 	<!-- CSS here -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="assets/css/slicknav.css">
-    <link rel="stylesheet" href="assets/css/flaticon.css">
-    <link rel="stylesheet" href="assets/css/progressbar_barfiller.css">
-    <link rel="stylesheet" href="assets/css/gijgo.css">
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-    <link rel="stylesheet" href="assets/css/animated-headline.css">
-	<link rel="stylesheet" href="assets/css/magnific-popup.css">
-	<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
-	<link rel="stylesheet" href="assets/css/themify-icons.css">
-	<link rel="stylesheet" href="assets/css/slick.css">
-	<link rel="stylesheet" href="assets/css/nice-select.css">
-	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/progressbar_barfiller.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/gijgo.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animated-headline.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/themify-icons.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/nice-select.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
 <body>
     <!-- ? Preloader Start -->
@@ -302,27 +302,27 @@
     </section>
     <!-- Pricing Card End -->
     <!-- Latest Offers Start -->
-    <div class="latest-wrapper">
-        <div class="container">
-            <div class="latest-area latest-height  section-bg2" data-background="assets/img/gallery/section_bg01.png">
-                <div class="row  align-items-center justify-content-between">
-                    <div class="col-xl-5 col-lg-7">
-                        <div class="latest-caption">
-                            <h2>Check our unbelievable super  fast Broadband availability in your area.</h2>
-                        </div>
-                    </div>
-                     <div class="col-xl-5 col-lg-5 col-md-10 ">
-                        <div class="latest-subscribe">
-                            <form action="#">
-                                <input type="email" placeholder="Enter Zipcode">
-                                <button>Check Now</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--    <div class="latest-wrapper">--}}
+{{--        <div class="container">--}}
+{{--            <div class="latest-area latest-height  section-bg2" data-background="assets/img/gallery/section_bg01.png">--}}
+{{--                <div class="row  align-items-center justify-content-between">--}}
+{{--                    <div class="col-xl-5 col-lg-7">--}}
+{{--                        <div class="latest-caption">--}}
+{{--                            <h2>Check our unbelievable super  fast Broadband availability in your area.</h2>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                     <div class="col-xl-5 col-lg-5 col-md-10 ">--}}
+{{--                        <div class="latest-subscribe">--}}
+{{--                            <form action="#">--}}
+{{--                                <input type="email" placeholder="Enter Zipcode">--}}
+{{--                                <button>Check Now</button>--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <!-- Latest Offers End -->
     <!--? About-2 Area Start -->
     <section class="about-area2 testimonial-area section-padding30 fix">
@@ -576,46 +576,46 @@
 
     <!-- JS here -->
 
-    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="{{asset('/assets/js/vendor/modernizr-3.5.0.min.js')}}"></script>
     <!-- Jquery, Popper, Bootstrap -->
-    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-    <!-- Jquery Mobile Menu -->
-    <script src="./assets/js/jquery.slicknav.min.js"></script>
+    <script src="{{asset('/assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{asset('/assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('/assets/js/bootstrap.min.js')}}"></script>
+    <!-- Jquery Mbile Menu -->
+    <script src="{{asset('/assets/js/jquery.slicknav.min.js')}}"></script>
 
-    <!-- Jquery Slick , Owl-Carousel Plugins -->
-    <script src="./assets/js/owl.carousel.min.js"></script>
-    <script src="./assets/js/slick.min.js"></script>
-    <!-- One Page, Animated-HeadLin -->
-    <script src="./assets/js/wow.min.js"></script>
-    <script src="./assets/js/animated.headline.js"></script>
-    <script src="./assets/js/jquery.magnific-popup.js"></script>
+    <!-- Jquery Sick , Owl-Carousel Plugins -->
+    <script src="{{asset('/assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('/assets/js/slick.min.js')}}"></script>
+    <!-- One Page Animated-HeadLin -->
+    <script src="{{asset('/assets/js/wow.min.js')}}"></script>
+    <script src="{{asset('/assets/js/animated.headline.js')}}"></script>
+    <script src="{{asset('/assets/js/jquery.magnific-popup.js')}}"></script>
 
-    <!-- Date Picker -->
-    <script src="./assets/js/gijgo.min.js"></script>
-    <!-- Nice-select, sticky -->
-    <script src="./assets/js/jquery.nice-select.min.js"></script>
-    <script src="./assets/js/jquery.sticky.js"></script>
-    <!-- Progress -->
-    <script src="./assets/js/jquery.barfiller.js"></script>
+    <!-- Date Picer -->
+    <script src="{{asset('/assets/js/gijgo.min.js')}}"></script>
+    <!-- Nice-selct, sticky -->
+    <script src="{{asset('/assets/js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('/assets/js/jquery.sticky.js')}}"></script>
+    <!-- Progress-->
+    <script src="{{asset('/assets/js/jquery.barfiller.js')}}"></script>
 
-    <!-- counter , waypoint,Hover Direction -->
-    <script src="./assets/js/jquery.counterup.min.js"></script>
-    <script src="./assets/js/waypoints.min.js"></script>
-    <script src="./assets/js/jquery.countdown.min.js"></script>
-    <script src="./assets/js/hover-direction-snake.min.js"></script>
+    <!-- counter  waypoint,Hover Direction -->
+    <script src="{{asset('/assets/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('/assets/js/waypoints.min.js')}}"></script>
+    <script src="{{asset('/assets/js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('/assets/js/hover-direction-snake.min.js')}}"></script>
 
-    <!-- contact js -->
-    <script src="./assets/js/contact.js"></script>
-    <script src="./assets/js/jquery.form.js"></script>
-    <script src="./assets/js/jquery.validate.min.js"></script>
-    <script src="./assets/js/mail-script.js"></script>
-    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+    <!-- contact s -->
+    <script src="{{asset('/assets/js/contact.js')}}"></script>
+    <script src="{{asset('/assets/js/jquery.form.js')}}"></script>
+    <script src="{{asset('/assets/js/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('/assets/js/mail-script.js')}}"></script>
+    <script src="{{asset('/assets/js/jquery.ajaxchimp.min.js')}}"></script>
 
-    <!-- Jquery Plugins, main Jquery -->
-    <script src="./assets/js/plugins.js"></script>
-    <script src="./assets/js/main.js"></script>
+    <!-- Jquery Pugins, main Jquery -->
+    <script src="{{asset('/assets/js/plugins.js')}}"></script>
+    <script src="{{asset('/assets/js/main.js')}}"></script>
 
     </body>
 </html>
